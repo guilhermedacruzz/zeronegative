c2247514cdfd7f8ed17f24014bb62cfe8ed0cfe0
indexGuilherme <guidacruz2004@gmail.com>
2021-12-05 10:17:07 -0300

    removendo a tirinha e colocando o read more

---
+++ scripts/read_more.js
@@ -0,0 +1,6 @@
+let icon = document.querySelector(".bi-arrow-down-short")
+let button = document.querySelector(".button-read-more")
+
+button.addEventListener("click", () => {
+    icon.classList.toggle("inverter")
+})
